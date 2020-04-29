35fad54291af05a4f33239b1dd63ab5e0ce5ae78
Kathy Luu <kathyluu820@gmail.com>
2020-04-28 21:57:21 -0700

    editing published page

--- src/App.js
+++ src/App.js
@@ -5,41 +5,13 @@ import ScrollBasedBezier from './components/ScrollBasedBezier';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        oh hello,
-        <ScrollBasedBezier headerHeight={10} />
-      </header>
-
       <div className="main__content">
-        <h1>I'm Kathy.</h1>
-        <p>
-          I am a software engineer based in San Jose, California. I enjoy
-          working with React on the front end. I am currently employed at
-          Intuit, Inc. in Mountain View. Oh, and I have a dog; her name is Pim.
-        </p>
-
-        <h3>contact</h3>
-        <ul>
-          <li>email: kathyluu820@gmail.com</li>
-        </ul>
-
-        <h3>experience</h3>
-        <ul>
-          <li>
-            <div>Intuit</div>
-            <div>2020 – present</div>
-          </li>
-          <li>
-            <div>Doctor.com</div>
-            <div>2017 – 2020</div>
-          </li>
-        </ul>
-
-        <h3>projects</h3>
-        <ul>
-          <li>Two Half-Hitches</li>
-          <li>Murakami Wedding</li>
-        </ul>
+        <h1>
+          under construction. check back soon, thanks{' '}
+          <span role="img" aria-label="smile">
+            😊
+          </span>
+        </h1>
       </div>
     </div>
   );
